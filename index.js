814ca61edd29838ac05c7add2803293dcdc2c298
jordi <jordi.lanau@yahoo.com>
2022-01-06 14:07:33 +0000

    Add questions

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'Enter your project title:',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Enter your project description:',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Enter your project installation instructions:',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Enter your project usage information:',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Enter your project contribution guidelines:',
+    name: 'contribution',
+  },
+  {
+    type: 'input',
+    message: 'Enter your project test instructions:',
+    name: 'test',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+  try {
+    const answers = await inquirer.prompt(questions);
+    console.log(answers);
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+}
 
 // Function call to initialize app
 init();
